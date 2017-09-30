61f004b7ebe281f0e1f302329c4be6e22c2dd5e1
MoonEyes <ducoinbenjamin@yahoo.fr>
2017-09-30 21:25:01 +0200

    Delete test.js

--- test/ex/test.js
+++
@@ -1,8 +0,0 @@
-var request = require('supertest');
-var app = require('/nant-it/app/index.js');
- 
-describe('GET /', function() {
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
-  });
-});
